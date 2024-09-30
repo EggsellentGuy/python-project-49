be559149c0203ea798753e2ee32f5da349f2c32c
EggsellentGuy <ivanrossi112@gmail.com>
2024-09-30 10:01:29 +0300

    add some new

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -13,12 +13,10 @@ def Generate_question():
 
 
 def is_prime_number(num: int) -> bool:
-    if num <= 1:
+    if num <= 1 or num % 2 == 0 or num % 3 == 0:
         return False
     if num <= 3:
         return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
     i = 5
     while i * i <= num:
         if num % i == 0 or num % (i + 2) == 0:
